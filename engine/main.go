7d4ca69f582f3402758867b3dfb05761862beb00
bytes <therealbyt3s@gmail.com>
2023-06-04 16:21:53 +0200

    feat: add pc metadata

--- engine/main.go
+++ engine/main.go
@@ -5,10 +5,17 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/concrete"
+	"github.com/ethereum/go-ethereum/concrete/precompiles"
 )
 
 func setup(engine concrete.ConcreteApp) {
-	engine.AddPrecompile(common.HexToAddress("0x80"), pcs.NESPrecompile)
+	engine.AddPrecompile(common.HexToAddress("0x80"), pcs.NESPrecompile, precompiles.PrecompileMetadata{
+		Name:        "NES",
+		Version:     "0.1.0",
+		Author:      "therealbytes",
+		Description: "Run activity on top of a given NES state",
+		Source:      "https://github.com/therealbytes/neschain/blob/main/engine/pcs/nes.go",
+	})
 }
 
 func main() {

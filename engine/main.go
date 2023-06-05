4166b12cce64eb6fd6d0d5b29e9b8d95e7b6b803
bytes <therealbyt3s@gmail.com>
2023-06-05 18:08:07 +0200

    feat: config concrete

--- engine/main.go
+++ engine/main.go
@@ -9,9 +9,19 @@ import (
 )
 
 func setup(engine concrete.ConcreteApp) {
+	engine.SetConfig(concrete.ConcreteConfig{
+		PreimageRegistryConfig: precompiles.PreimageRegistryConfig{
+			Enabled:  true,
+			Writable: true,
+		},
+		BigPreimageRegistryConfig: precompiles.PreimageRegistryConfig{
+			Enabled:  true,
+			Writable: true,
+		},
+	})
 	engine.AddPrecompile(common.HexToAddress("0x80"), pcs.NESPrecompile, precompiles.PrecompileMetadata{
 		Name:        "NES",
-		Version:     "0.1.0",
+		Version:     precompiles.Version{common.Big0, common.Big1, common.Big0},
 		Author:      "therealbytes",
 		Description: "Run activity on top of a given NES state",
 		Source:      "https://github.com/therealbytes/neschain/blob/main/engine/pcs/nes.go",
